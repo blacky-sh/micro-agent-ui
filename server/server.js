b4d8f647f27f7d7f88367db3d702eb04b8932ef9
Sheehab <shehabedinali@gmail.com>
2024-11-17 15:27:36 +0300

    feat: Setup Express server and MongoDB connection

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+
+const port = process.env.port 
+const pwd = process.env.MONGOPWD
+const usr = process.env.MONGOUSR
+
+const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://${usr}:${pwd}@backenddb.dxxef.mongodb.net/micro-agent?retryWrites=true&w=majority&appName=backendDb`
+  )
+  .then(() => {
+    console.log("Connected to the database!");
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+  })
+  .catch(() => console.log("Connection Failed!"));
