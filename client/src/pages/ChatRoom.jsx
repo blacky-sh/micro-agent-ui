6baac043a0e5a4513853a82106b45489f16939d4
Sheehab <shehabedinali@gmail.com>
2024-11-17 10:11:57 +0300

    feat: Add authentication check and redirect to settings if setting not set

--- client/src/pages/ChatRoom.jsx
+++ client/src/pages/ChatRoom.jsx
@@ -1,16 +1,36 @@
-import React from "react";
-import ChatBox from "../components/ChatBox";
-import SendMessage from "../components/SendMessage";
-import { MessagesProvider } from "../MessagesContext";
+import React, { useState, useEffect } from 'react';
+import ChatBox from '../components/ChatBox';
+import SendMessage from '../components/SendMessage';
+import { MessagesProvider } from '../MessagesContext';
+import { useNavigate } from 'react-router-dom';
 
 const ChatRoom = () => {
+  const [isSet, setIsSet] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedApiProvider = localStorage.getItem('apiProvider');
+    const savedApiKey = localStorage.getItem('apiKey');
+    const savedModel = localStorage.getItem('model');
+
+    if (savedApiProvider && savedApiKey && savedModel) {
+      setIsSet(true);
+    } else {
+      navigate('/setting');
+    }
+  }, [navigate]); // Add navigate to the dependency array
+
   return (
-    <MessagesProvider>
-      <div>
-        <ChatBox />
-        <SendMessage />
-      </div>
-    </MessagesProvider>
+    <>
+      {isSet ? (
+        <MessagesProvider>
+          <div>
+            <ChatBox />
+            <SendMessage />
+          </div>
+        </MessagesProvider>
+      ) : null}
+    </>
   );
 };
 
